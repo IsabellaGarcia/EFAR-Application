db3996454cc4723ab7e578c1f3ae5719a391fd81
IsabellaGarcia <isabella911217@gmail.com>
2014-12-06 12:55:10 +0800

    EventAcitivity Receive only 1 SMS succesfully

--- EFARApplication/src/com/mobile/efar/activity/EventActivity.java
+++ EFARApplication/src/com/mobile/efar/activity/EventActivity.java
@@ -46,11 +46,49 @@ public class EventActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setTitle("Emergency Events List");
-		setContentView(R.layout.event_list_view); 
+		setContentView(R.layout.event_detail); 
 		Layout();
 	}
 	
-	
+ 	protected void onStart(){
+		super.onStart();
+		List<EventModel> list = new ArrayList<EventModel>();
+		
+		Bundle extras = getIntent().getExtras();
+		ArrayList<String> data = null;
+		if(extras != null){
+			data = extras.getStringArrayList("message");
+			for(int j = 0; j<data.size(); j++){
+				//Split into single SMS
+				String[] messeges = data.get(j).split("&");
+				/*
+				 * Split one SMS
+				 * Garcia#2014-11-05 12:17:00#@Central #Event: There is...... &
+				 * info[0]: sender
+				 * info[1]: time
+				 * info[2]: @tag
+				 * info[3]: body
+				 */
+				String[] info = messeges[0].split("#");
+				EventModel event1 = new EventModel();
+				//NEED TO MODIFY
+				int i = 0;
+				event1.setId(i);
+				event1.setPhone(info[0]);
+				senderName = (TextView)findViewById(R.id.sender_name);
+				senderName.setText(event1.getPhone());
+				event1.setTime(info[1]);
+				sendTime = (TextView)findViewById(R.id.send_time);
+				sendTime.setText(event1.getTime());
+				event1.setAddress_tag(info[2]);
+				address_tag = (TextView)findViewById(R.id.address_tag);
+				address_tag.setText(event1.getAddress_tag());
+				event1.setDescription(info[3]);
+				body = (TextView)findViewById(R.id.body);
+				body.setText(event1.getDescription());
+			}	
+		}		
+	}
 
 	private List<EventModel> getData() {
 		List<EventModel> list = new ArrayList<EventModel>();
@@ -65,10 +103,10 @@ public class EventActivity extends Activity{
 	
 	//Initialize the layout of this activity
 	private void Layout(){
-		 lv_list = (ListView) findViewById(R.id.event_list);
+		/* lv_list = (ListView) findViewById(R.id.event_list);
 		 mAdapter = new EventAdapter(this, getData());
 		 lv_list.setAdapter(mAdapter);
-		 container = (FrameLayout) findViewById(R.id.framelayout_main);
+		 container = (FrameLayout) findViewById(R.id.framelayout_main);*/
 		 
 		//Click to open a record list
 		imagebutton_record = (ImageButton) findViewById(R.id.button_record);
